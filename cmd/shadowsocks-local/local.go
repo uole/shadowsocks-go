c678ab1aa58cab715f6a3354ec9e8d4ddd848770
lxg <fancl@cc.com>
2018-11-15 10:53:49 +0800

    change package names

--- cmd/shadowsocks-local/local.go
+++ cmd/shadowsocks-local/local.go
@@ -17,7 +17,7 @@ import (
 	"strings"
 	"time"
 
-	ss "github.com/shadowsocks/shadowsocks-go/shadowsocks"
+	ss "github.com/uole/shadowsocks-go/shadowsocks"
 )
 
 var debug ss.DebugLog
--- cmd/shadowsocks-server/server.go
+++ cmd/shadowsocks-server/server.go
@@ -19,7 +19,7 @@ import (
 	"syscall"
 	"time"
 
-	ss "github.com/shadowsocks/shadowsocks-go/shadowsocks"
+	ss "github.com/uole/shadowsocks-go/shadowsocks"
 )
 
 const (
